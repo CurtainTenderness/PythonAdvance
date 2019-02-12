eb27a517a477e9461f0aa223365de0f42170bee4
lyf <2598895690.com>
2019-02-12 22:03:12 +0800

    习题

---
+++ day0212/text03.py
@@ -0,0 +1,89 @@
+# 1，编写代码验证
+# 类可以操作类属性，不可以操作实例属性
+# 实例可以操作实例属性，可以操作类属性
+# 2，新建Shop类
+# 编写类说明
+# 编写类属性isopen（是否开张）
+# 编写实例属性shoplist（ 商品列表）
+# 编写类方法 返回字符串为类说明
+# 编写实例方法添加商品方法
+# 编写实例方法获取商品列表方法
+# 编写静态方法功能随意
+# 3，编写代码验证
+# 动态的添加类属性
+# 动态的添加实例属性
+# 说明类与实例以及类属性实例属性相互调用关系
+
+
+
+
+# # 第一题
+# class person(object):
+#     name="ljk"
+#     def years(self,_age):
+#         self.age=_age
+# q1=person()
+# print(q1.name)
+# q1.old=18
+# person.name="ljk1"
+# print(q1.name)
+# person.old=20
+# print(q1.old)
+# q1.old=23
+# print(q1.old)
+# q1.name="ljk2"
+# print(q1.name)
+
+
+# 第二题
+# class Shop(object):
+#     """
+# 商店是每个城市的，方便了人们的生活
+#
+#     """
+#     isopen=True
+#     def shoplist(self,_name,_price):
+#         self.name=_name
+#         self.price=_price
+#
+#     @classmethod
+#     def printclassinfo(cls):
+#         print(cls.__doc__)
+#     @staticmethod
+#     def information():
+#         print("这是一个商店")
+#
+# s1=Shop()
+# print(s1.isopen)
+# s1.shoplist("苹果","10元")
+# print(s1.name)
+# s1.printclassinfo()
+# s1.time="3个月"
+# print(s1.time)
+# print(s1.name,s1.price,s1.time)
+# s1.information()
+
+
+
+# 第三题
+from day0212.utils import Utils
+print(Utils.max(5,100))
+class Person(object):
+    def __init__(self, _name):
+        self.name = _name
+    def setname(self,  _newname):
+        self.name = _newname
+@classmethod
+def time(cls):
+    print("time")
+@staticmethod
+def log():
+    print("disanti")
+s1=Person("weg")
+Person.age=18
+s1.time=time
+print(s1.time)
+print(s1.age)
+
+
+
---
+++ day0212/utils.py
@@ -0,0 +1,11 @@
+class Utils(object):
+    @staticmethod
+    def max(a, b):
+        if a > b:
+            return a
+        else:
+            return b
+
+    @staticmethod
+    def power(a, b):
+        return a ** b
