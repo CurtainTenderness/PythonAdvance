8636dd32dc49aaecb95b90f442c5a8aee66da318
lyf <2598895690.com>
2019-02-19 16:51:29 +0800

    个线程之间执行没有固定的先后顺序，需要抢占CPU资源

---
+++ day0219/text0219-6.py
@@ -0,0 +1,18 @@
+"""
+线程
+多个线程之间执行没有固定的先后顺序，需要抢占CPU资源
+"""
+from threading import Thread
+import time
+def threadmain(name):
+    while True:
+        time.sleep(1)
+        print(name,"执行了")
+def main():
+    tread1 = Thread(target=threadmain, args="线程1")
+    tread2 = Thread(target=threadmain, args="线程2")
+    tread1.start()
+    tread2.start()
+
+if __name__=="__main__":
+    main()
