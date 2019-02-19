9b98af41caf07db64dfac894946fedef1f8efe6f
lyf <2598895690.com>
2019-02-19 20:43:22 +0800

    多线程下载与单线程下载的对比

--- day0219/text0219-5.py
+++ day0219/text0219-5.py
@@ -4,7 +4,7 @@
 import time
 from threading import Thread
 def prt():
-    "模拟耗时操作"
+    "模拟耗时操作，比如下载资源"
     time.sleep(1)
     print("+")
 def singspendtime():
@@ -14,17 +14,24 @@ def singspendtime():
         prt()
     end = time.time()
     print("时间消耗", end - start)
-def threadmain(num):
-    print(num)
+def threadmain():
+    prt()
 def multithread():
     treadlist=[]
     "创建线程"
     start=time.time()
     for r in range(5):
-        t1 = Thread(name="multithread", target=threadmain,args=(10,))
+        t1 = Thread(name="multithread", target=threadmain)
         t1.start()
-        print(t1.name)
         treadlist.append(t1)
+        print(t1.name)
+    # 主进程执行完毕，子进程还没有执行完
+    # 如果想要统计时间消耗需要等待五个子进程执行完毕
+    # 确保每个子线程都join才能统计结束时间
+
+    # 阻塞主进程（需要的子线程都已经创建成功）
+    for t in treadlist:
+        t.join()
     end=time.time()
 
     print(end-start)
