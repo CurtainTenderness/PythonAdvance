1e25dfb2f50586285862c715ef8360b38c9cec12
lyf <2598895690.com>
2019-02-19 21:10:21 +0800

    多线程-内存共享

--- day0219/day0219-7.py
+++ day0219/day0219-7.py
@@ -8,7 +8,7 @@ from threading import Thread
 def threadmain():
     global num
     num=20
-    print("子线程",num)
+    print("子线程",num,id(num))
 
 
 
@@ -18,9 +18,11 @@ if __name__=="__main__":
     thread.start()
     thread.join()
     print("finish")
-    print(num)
+    print("主线程",num,id(num))
 
 """
+在函数内部赋值的局部变量 不能使用global访问到
+多个线程可以共享主线程数据
 多线程：内存独立，共享数据需要用queue
 多进程：内存不独立，本身就能共享数据
 """
