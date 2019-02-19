7bd2ec95668a346288937f55f70c1494f3d37b80
lyf <2598895690.com>
2019-02-19 20:59:09 +0800

    多线程-执行无序

--- day0219/text0219-6.py
+++ day0219/text0219-6.py
@@ -1,6 +1,7 @@
 """
 线程
 多个线程之间执行没有固定的先后顺序，需要抢占CPU资源
+其实python多线程伪多线程，本质并不是并行的
 """
 from threading import Thread
 import time
@@ -9,8 +10,8 @@ def threadmain(name):
         time.sleep(1)
         print(name,"执行了")
 def main():
-    tread1 = Thread(target=threadmain, args="线程1")
-    tread2 = Thread(target=threadmain, args="线程2")
+    tread1 = Thread(target=threadmain, args=("线程1",))
+    tread2 = Thread(target=threadmain, args=("线程2",))
     tread1.start()
     tread2.start()
 
